0c1b1733d480ed76c710d23afc97c2b2bcfb8f12
aya-0p <4587game.aya@gmail.com>
2022-08-08 16:02:04 +0900

    Update editor.js
    #2

---
+++ res/editor.js
@@ -0,0 +1,27 @@
+window.addEventListener("keydown", (e) => {
+  const code = e.code;
+  switch (code) {
+    case "ArrowUp":
+      window.editor.up()
+      break;
+    case "ArrowDown":
+      window.editor.down()
+      break;
+    case "ArrowLeft":
+      window.editor.left()
+      break;
+    case "ArrowRight":
+      window.editor.right()
+      break;
+    case "Enter":
+      window.editor.enter()
+      break;
+    default:
+  }
+});
+window.editor.updateFunc((_, buffer) => {
+  let blob = new Blob([buffer], { type: "image/png" });
+  let urlCreator = window.URL || window.webkitURL;
+  let src = urlCreator.createObjectURL(blob);
+  document.getElementById("image").src = src
+})
